299821dd9c18053b71c9656819926a5f110bd3bf
Tue Gyhrs <tugy@itu.dk>
2020-11-27 17:57:07 +0100

    Sync

--- src/com/langesokker/Reader.java
+++ src/com/langesokker/Reader.java
@@ -1,39 +1,33 @@
 package com.langesokker;
 
 import com.langesokker.media.Film;
-import com.langesokker.media.Media;
-
 import java.io.*;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
-import java.util.Scanner;
+
 
 public class Reader {
-
     public static void main(String[] args) {
         new Reader();
     }
 
     public Reader() {
+        //Handles no file exception
         ClassLoader loader = getClass().getClassLoader();
-        //Handles null file exception
         if (loader == null) {
             // TODO: make it throw exception...
             return;
         }
+
         BufferedReader reader = null;
-
         try {
-            String CurrentLine;
             reader = new BufferedReader(new FileReader("src/com/langesokker/data/film.txt"));
-
+            String CurrentLine;
             while ((CurrentLine = reader.readLine()) != null) {
-                System.out.println(CurrentLine);
+                    System.out.println(CurrentLine);
 
-                String[] currentFilm = CurrentLine.split(";");
-                new Film(currentFilm[0], Integer.parseInt(currentFilm[1]), currentFilm[2].replaceAll("\\s+", "").split(","), Double.parseDouble(currentFilm[3]), "");
-            }
+                    String[] currentFilm = CurrentLine.split(";");
+                    new Film(currentFilm[0], Integer.parseInt(currentFilm[1]), currentFilm[2].replaceAll("\\s+", "").split(","), Double.parseDouble(currentFilm[3]), "");
+
+                }
         } catch (IOException e) {
             e.printStackTrace();
         } finally {
