7c12cbc56231c0515f6bac745cf604657e48b09e
Philip Flyvholm <phil14052@gmail.com>
2020-12-07 15:37:57 +0100

    Async loading

--- src/com/langesokker/views/FrontPageView.java
+++ src/com/langesokker/views/FrontPageView.java
@@ -4,6 +4,7 @@ import com.langesokker.components.containers.MediaItemContainer;
 import com.langesokker.components.containers.NavBarContainer;
 import com.langesokker.controllers.MediaController;
 import com.langesokker.media.Media;
+import com.langesokker.media.MediaLoaderTask;
 import com.langesokker.media.SupportedMediaTypes;
 import com.langesokker.utils.Colors;
 
@@ -12,6 +13,7 @@ import javax.swing.border.Border;
 import java.awt.*;
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
 
 public class FrontPageView extends BaseView{
     private final MediaController mediaController = MediaController.getInstance();
@@ -97,53 +99,11 @@ public class FrontPageView extends BaseView{
      * Updaterer containeren af medier baseret på den string af karakterer man søger efter
      */
     private JScrollPane updateContentContainer(String mediaTypeString, String genre, String query){
-        SupportedMediaTypes mediaType = null;
-        try {
-            mediaType = SupportedMediaTypes.valueOf(mediaTypeString);
-        } catch (IllegalArgumentException ignored) {}
-
-        int item = 0;
         JPanel panel = new JPanel();
-        panel.setLayout(new BoxLayout(panel, BoxLayout.PAGE_AXIS));
-        panel.setBackground(Colors.SECONDARY_DARK.getColor());
-        Container rowContainer = new Container();
-        rowContainer.setLayout(new FlowLayout(FlowLayout.CENTER, 20, 5));
-        for(List<Media> mediaList : mediaController.getMediaMap().values()){
-            for (Media media : mediaList){
-                if (!mediaTypeString.trim().equals("")
-                        && !mediaTypeString.equalsIgnoreCase("All media")) {
-                        if (mediaType == null || !media.getType().equals(mediaType)) {
-                            continue;
-                        }
-                }
-
-                if (!genre.equals("")
-                        && !genre.equalsIgnoreCase("All genres")) {
-                    List<String> genreList = Arrays.asList(media.getGenres());
-                    if(!genreList.contains(genre)) {
-                        continue;
-                    }
-                }
-
-                if(!media.getName().toLowerCase().contains(query.toLowerCase()) && !query.equals("")){
-                    continue;
-                }
-                if(item % 5 == 0){
-                    panel.add(rowContainer);
-                    rowContainer = new Container();
-                    rowContainer.setLayout(new FlowLayout(FlowLayout.CENTER, 20, 5));
-
-                }
-                rowContainer.add(new MediaItemContainer(media).getContainer());
-
-                item++;
-            }
-        }
-
-        panel.add(rowContainer);
-
         JScrollPane scrollPane = new JScrollPane(panel, ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED, ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
-        scrollPane.getVerticalScrollBar().setUnitIncrement(10);
+        scrollPane.getVerticalScrollBar().setUnitIncrement(15);
+        MediaLoaderTask task = new MediaLoaderTask(panel, mediaController.getMediaMap(), mediaTypeString, genre, query);
+        task.execute();
         return scrollPane;
     }
 }
